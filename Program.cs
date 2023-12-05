f97a369900fa831ee9acb6ffc194972a8fd3bb30
Kendall Walker <148366302+knwalker42@users.noreply.github.com>
2023-12-05 16:51:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //filler for now
+    }
+    static int Add()
+    {
+        //filler
+    }
+    static int Subtract()
+    { 
+        //filler    
+    }
+    static int Multiply()
+    {
+        //filler
+    }
+    static int Divide()
+    {
+        //filler
+    }
+}
